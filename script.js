70ddaf746e036488476658fbd62c946d72c85c3d
gusjoh <gustav_johansson95@outlook.com>
2022-09-19 15:38:49 +0200

    Created basic canvas template

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 "use strict";
+let canvasElement = null;
+let ctx = null;
+window.addEventListener("load", function(){ 
+    canvasElement = document.querySelector("#canvasElement");
+    ctx = canvasElement.getContext("2d", { alpha: false });
+    window.requestAnimationFrame(renderCanvas);
+    setCanvasSize();
+});
+window.addEventListener("resize", setCanvasSize);
+
+function setCanvasSize(){
+	canvasElement.width = window.innerWidth;
+	canvasElement.height = window.innerHeight;
+	instantiateObjects();
+}
+
+function instantiateObjects(){
+
+}
+
+function renderCanvas(){
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+    window.requestAnimationFrame(renderCanvas);
+}
